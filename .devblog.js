68e31a9c6fc5dcb6581ff0172069fc4a261e55a3
Christian Fei <crifei93@gmail.com>
2020-10-13 09:04:50 +0200

    42 subs

--- .devblog.js
+++ .devblog.js
@@ -3,7 +3,7 @@ const today = new Date().toISOString().substring(0, 10)
 module.exports = {
   ignoredFiles: ['gulpfile.js', '._includes', 'scss', 'LICENSE', 'README.md'],
   collections: {
-    subscribers: 36
+    subscribers: 42
   },
   nunjucksFilters: [{
     name: 'year',
